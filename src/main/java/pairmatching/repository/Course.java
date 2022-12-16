96bcb8659571203ded7a788d2faa181c1b98a400
Youngho <cyh6099@gmail.com>
2022-12-16 13:04:00 +0900

    feat : 코스 입력에 대해 존재하는 코스인지 검사하는 기능 구현

--- src/main/java/pairmatching/repository/Course.java
+++ src/main/java/pairmatching/repository/Course.java
@@ -1,12 +1,26 @@
 package pairmatching.repository;
 
+import java.util.Arrays;
+
 public enum Course {
     BACKEND("백엔드"),
     FRONTEND("프론트엔드");
 
-    private String name;
+    private final String name;
 
     Course(String name) {
         this.name = name;
+    }
+
+    public static boolean of(String courseName) {
+        Arrays.stream(Course.values())
+                .filter(course -> course.getName().equals(courseName))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 코스입니다."));
+        return true;
+    }
+
+    public String getName() {
+        return name;
     }
 }
