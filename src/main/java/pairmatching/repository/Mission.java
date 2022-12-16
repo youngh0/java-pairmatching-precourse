130d76a35593c278b43fcb2e38ff639fbbcf211c
Youngho <cyh6099@gmail.com>
2022-12-16 15:40:24 +0900

    refactor : 미션에 해당하는 레벨을 enum으로 변경

--- src/main/java/pairmatching/repository/Mission.java
+++ src/main/java/pairmatching/repository/Mission.java
@@ -3,26 +3,26 @@ package pairmatching.repository;
 import java.util.Arrays;
 
 public enum Mission {
-    RACING("자동차경주","레벨1"),
-    LOTTO("로또", "레벨1"),
-    BASEBALL("숫자야구게임", "레벨1"),
-    SHOPPING_CART("장바구니", "레벨2"),
-    PAYMENT("결제", "레벨2"),
-    SUBWAY("지하철노선도", "레벨2"),
-    REFACTORING("성능개선", "레벨4"),
-    DEPLOY("배포", "레벨4");
+    RACING("자동차경주",Level.LEVEL1),
+    LOTTO("로또", Level.LEVEL1),
+    BASEBALL("숫자야구게임", Level.LEVEL1),
+    SHOPPING_CART("장바구니", Level.LEVEL2),
+    PAYMENT("결제", Level.LEVEL2),
+    SUBWAY("지하철노선도", Level.LEVEL2),
+    REFACTORING("성능개선", Level.LEVEL4),
+    DEPLOY("배포", Level.LEVEL4);
 
     private final String missionName;
-    private final String level;
+    private final Level level;
 
-    Mission(String missionName, String level) {
+    Mission(String missionName, Level level) {
         this.missionName = missionName;
         this.level = level;
     }
 
-    public static boolean of(String missionName, String level) {
+    public static boolean of(String missionName, Level level) {
         Arrays.stream(Mission.values())
-                .filter(mission -> missionName.equals(mission.missionName) && level.equals(mission.level))
+                .filter(mission -> mission.missionName.equals(missionName) && mission.level.equals(level))
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 미션입니다."));
         return true;
--- src/test/java/pairmatching/repository/MissionTest.java
+++ src/test/java/pairmatching/repository/MissionTest.java
@@ -2,20 +2,20 @@ package pairmatching.repository;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class MissionTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"자동차경주:레벨2", "야구게임:레벨1"}, delimiter = ':')
-    void 유효한_미션_예외_테스트(String missionName, String level) {
+    @ValueSource(strings = {"자동차경주", "야구게임"})
+    void 유효한_미션_예외_테스트(String missionName) {
         Assertions.assertThrows(IllegalArgumentException.class,
-                () -> Mission.of(missionName, level));
+                () -> Mission.of(missionName, Level.LEVEL2));
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"자동차경주:레벨1", "숫자야구게임:레벨1"}, delimiter = ':')
-    void 유효한_미션_테스트(String missionName, String level) {
-        org.assertj.core.api.Assertions.assertThat(Mission.of(missionName, level)).isEqualTo(true);
+    @ValueSource(strings = {"자동차경주", "숫자야구게임"})
+    void 유효한_미션_테스트(String missionName) {
+        org.assertj.core.api.Assertions.assertThat(Mission.of(missionName, Level.LEVEL1)).isEqualTo(true);
     }
 }
