fa83517c4d9d6fbda83543a400e7818771fe5d5e
Youngho <cyh6099@gmail.com>
2022-12-16 15:19:06 +0900

    feat : 섞인 크루원 기반 페어 생성 기능 구현

--- src/main/java/pairmatching/service/MatchingService.java
+++ src/main/java/pairmatching/service/MatchingService.java
@@ -4,6 +4,7 @@ import pairmatching.repository.Course;
 import pairmatching.repository.Crew;
 import pairmatching.repository.CrewInfo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MatchingService {
@@ -15,5 +16,15 @@ public class MatchingService {
 
     public List<Crew> shuffleCrews(Course course) {
         return crewInfo.shuffleCourseCrews(course);
+    }
+
+    public void makePair(List<Crew> crews) {
+        List<List<Crew>> pairResult = new ArrayList<>();
+        int startIdx = 0;
+        for (int count = 0; count < crews.size() / 2 - 1; count++) {
+            pairResult.add(crews.subList(startIdx, startIdx + 2));
+            startIdx += 2;
+        }
+        pairResult.add(crews.subList(startIdx, crews.size()));
     }
 }
