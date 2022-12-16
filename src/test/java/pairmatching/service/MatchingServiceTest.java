35ab2242f8881258781e3c8b979518f35772fd96
Youngho <cyh6099@gmail.com>
2022-12-16 15:34:20 +0900

    test : 매칭 정보 저장, 미션 별 이미 생성된 매칭 정보가 있는지에 대한 테스트 작성

--- src/test/java/pairmatching/service/MatchingServiceTest.java
+++ src/test/java/pairmatching/service/MatchingServiceTest.java
@@ -2,9 +2,11 @@ package pairmatching.service;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import pairmatching.domain.MatchingInfo;
 import pairmatching.repository.Course;
 import pairmatching.repository.Crew;
 import pairmatching.repository.CrewInfo;
+import pairmatching.repository.Mission;
 import pairmatching.util.CrewInfoReader;
 
 import java.io.IOException;
@@ -12,7 +14,7 @@ import java.util.List;
 
 class MatchingServiceTest {
 
-    MatchingService matchingService = new MatchingService(new CrewInfo(new CrewInfoReader()));
+    MatchingService matchingService = new MatchingService(new CrewInfo(new CrewInfoReader()), new MatchingInfo());
     CrewInfoReader crewInfoReader = new CrewInfoReader();
     List<Crew> backendCrew;
     MatchingServiceTest() throws IOException {
@@ -23,5 +25,19 @@ class MatchingServiceTest {
     void 크루원정보_섞기_테스트() {
         List<Crew> crews = matchingService.shuffleCrews(Course.BACKEND);
         Assertions.assertThat(crews.equals(backendCrew)).isFalse();
+    }
+
+    @Test
+    void 매칭_정보_존재_여부_테스트() {
+        List<Crew> crews = matchingService.shuffleCrews(Course.BACKEND);
+        matchingService.saveMatchingInfo(Mission.BASEBALL, matchingService.makePair(crews));
+        Assertions.assertThat(matchingService.isExistMatching(Mission.BASEBALL)).isTrue();
+    }
+
+    @Test
+    void 매칭_정보_존재_여부_FALSE_테스트() {
+        List<Crew> crews = matchingService.shuffleCrews(Course.BACKEND);
+        matchingService.saveMatchingInfo(Mission.BASEBALL, matchingService.makePair(crews));
+        Assertions.assertThat(matchingService.isExistMatching(Mission.DEPLOY)).isFalse();
     }
 }
